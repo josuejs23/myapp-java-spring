23f607723e3045c78d4b83398efb985a60e83715
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-08-22 16:07:35 -0400

    Reading values from the application.properties

--- src/main/java/com/josuejs23/myapp/rest/FunRestController.java
+++ src/main/java/com/josuejs23/myapp/rest/FunRestController.java
@@ -1,12 +1,24 @@
 package com.josuejs23.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    public String coachName;
+
+    @Value("${team.name}")
+    public String teamName;
+
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello World";
+    }
+
+    @GetMapping("/teaminfo")
+    public String teamInfo(){
+        return "The team %s has the coach %s".formatted(teamName, coachName);
     }
 }
